e0ee4974db7e994ba49ccccc1489402b4482898f
Xarizmo <mpolikarpoff@gmail.com>
2020-03-17 09:34:23 +0300

    Refactoring and onAddTaskClick (3)

--- src/App.js
+++ src/App.js
@@ -5,28 +5,72 @@ import TodoListTasks from './TodoListTasks';
 import TodoListFooter from './TodoListFooter';
 
 class App extends React.Component {
-    tasks = [
+  
+  // constructor(props) {
+  //   super(props);
+  //   setTimeout(() => {
+  //     let newTask = {title: "JS", isDone: false, priority: "priority: medium"};
+  //     let newTasks = [...this.state.tasks, newTask];
+  //     this.setState({
+  //       tasks: newTasks
+  //     })
+  //   }, 2000);
+  // }
+  
+  newTaskTitleRef = React.createRef();
+  
+  state = {
+    tasks: [
       {title: "JS", isDone: false, priority: "priority: medium"},
       {title: "CSS", isDone: true, priority: "priority: low"},
       {title: "React", isDone: false, priority: "priority: high"},
       {title: "SaSS", isDone: true, priority: "priority: medium"},
       {title: "Redux", isDone: false, priority: "priority: high"}
-    ];
-    
-    filterValue = "Completed";
+    ],
+    filterValue: "Completed"
+  }
   
-    render = () => {
-        
-        return (
-            <div className="App">
-                <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
-                </div>
-            </div>
-        );
+  onAddTaskClick = () => {
+    let newTitle = this.newTaskTitleRef.current.value;
+    this.newTaskTitleRef.current.value = '';
+    let newTask = {
+      title: newTitle,
+      isDone: false,
+      priority: "priority: medium"
     };
+    let newTasks = [...this.state.tasks, newTask];
+    this.setState({
+      tasks: newTasks
+    })
+    
+  };
+  
+  render = () => {
+    
+    return (
+      <div className="App">
+        <div className="todoList">
+          {/*<TodoListHeader/>*/}
+          
+          <div className="todoList-header">
+            <h3 className="todoList-header__title">What to Learn</h3>
+            <div className="todoList-newTaskForm">
+              <input
+                ref={this.newTaskTitleRef}
+                type="text"
+                placeholder="New task name"
+              />
+              <button onClick={this.onAddTaskClick}>Add
+              </button>
+            </div>
+          </div>
+          
+          <TodoListTasks tasks={this.state.tasks}/>
+          <TodoListFooter filterValue={this.state.filterValue}/>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
